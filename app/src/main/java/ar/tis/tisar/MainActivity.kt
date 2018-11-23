1ebdb21008250011659ed08a8f343574e042e8f7
felixfuchs <felix_fuchs.17@live.de>
2018-11-23 20:36:50 +0100

    Permissions for Location an Camera

--- app/src/main/java/ar/tis/tisar/MainActivity.kt
+++ app/src/main/java/ar/tis/tisar/MainActivity.kt
@@ -3,7 +3,7 @@ package ar.tis.tisar
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import ar.tis.tisar.helper.CameraPermissionHelper
+import ar.tis.tisar.helper.ARLocationPermissionHelper
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,29 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ARLocationPermissionHelper.requestPermission(this)
     }
 
-    override fun onResume() {
-        super.onResume();
 
-        // ARCore requires camera permission to operate.
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            CameraPermissionHelper.requestCameraPermission(this);
-            return;
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, results: IntArray) {
-        if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
-                .show()
-            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
-                // Permission denied with checking "Do not ask again".
-                CameraPermissionHelper.launchPermissionSettings(this)
-            }
-            finish()
-        }
-    }
 
     
 
