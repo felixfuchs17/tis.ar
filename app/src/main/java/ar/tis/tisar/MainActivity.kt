f778982b6373cc60ddd68f33bcdf326c77fd6013
felixfuchs <felix_fuchs.17@live.de>
2018-11-23 19:30:27 +0100

    die zwei Fragments hinzugefügt

--- app/src/main/java/ar/tis/tisar/MainActivity.kt
+++ app/src/main/java/ar/tis/tisar/MainActivity.kt
@@ -1,13 +1,9 @@
 package ar.tis.tisar
 
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Toast
 import ar.tis.tisar.helper.CameraPermissionHelper
-import com.google.ar.core.ArCoreApk
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        maybeEnableArButton()
-
     }
 
     override fun onResume() {
@@ -42,20 +36,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun maybeEnableArButton() {
-        val availability = ArCoreApk.getInstance().checkAvailability(this)
-        if (availability.isTransient) {
-            // Re-query at 5Hz while compatibility is checked in the background.
-            Handler().postDelayed(Runnable { maybeEnableArButton() }, 200)
-        }
-        if (availability.isSupported) {
-            mArButton.setVisibility(View.VISIBLE)
-            mArButton.setEnabled(true)
-            // indicator on the button.
-        } else { // Unsupported or unknown.
-            mArButton.setVisibility(View.INVISIBLE)
-            mArButton.setEnabled(false)
-        }
-    }
+    
 
 }
