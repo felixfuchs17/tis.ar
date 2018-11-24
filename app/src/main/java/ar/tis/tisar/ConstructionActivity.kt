b74a0543bc2ddde1b697cc5382888821374e76c8
Patrick.Arens <Patrick.Arens@rwth-aachen.de>
2018-11-24 01:51:52 +0100

    edit layout

--- app/src/main/java/ar/tis/tisar/ConstructionActivity.kt
+++ app/src/main/java/ar/tis/tisar/ConstructionActivity.kt
@@ -1,7 +1,10 @@
 package ar.tis.tisar
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class ConstructionActivity : AppCompatActivity() {
 
@@ -9,4 +12,13 @@ class ConstructionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_construction_site)
     }
+
+    fun infoToChat(v: View) {
+        Log.e("TAG", "ONCLICK")
+        val intent = Intent(this@ConstructionActivity, ConstructionActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
 }
+
+
