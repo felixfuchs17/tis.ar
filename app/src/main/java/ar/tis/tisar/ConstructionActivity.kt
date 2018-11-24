06a9fc9e3c038f4136a304c9996befa34d98fc8d
Patrick.Arens <Patrick.Arens@rwth-aachen.de>
2018-11-24 05:03:29 +0100

    add change visibility function

--- app/src/main/java/ar/tis/tisar/ConstructionActivity.kt
+++ app/src/main/java/ar/tis/tisar/ConstructionActivity.kt
@@ -15,10 +15,34 @@ class ConstructionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_construction_site)
     }
 
-    fun changeVisibility() {
-        var textData: TextView = findViewById(R.id.V3) as TextView
+    var testData: BooleanArray = booleanArrayOf(true, true, false, false , false, false, true)
 
-        textData.visibility = View.VISIBLE
+    fun changeVisibility() {
+        var i: Int = 1
+        var textData: TextView
+        for(value: Boolean in testData) {
+            when(i) {
+                1 -> textData = findViewById(R.id.V1) as TextView
+                2 -> textData = findViewById(R.id.V2) as TextView
+                3 -> textData = findViewById(R.id.V3) as TextView
+                4 -> textData = findViewById(R.id.V4) as TextView
+                5 -> textData = findViewById(R.id.V5) as TextView
+                6 -> textData = findViewById(R.id.V6) as TextView
+                7 -> textData = findViewById(R.id.V7) as TextView
+                else -> textData = findViewById(R.id.V1) as TextView
+            }
+
+            if(value) {
+                textData.visibility = View.VISIBLE
+            } else {
+                textData.visibility = View.GONE
+            }
+
+            i++
+        }
+
+
+
     }
 }
 
